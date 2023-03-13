c255eaf2d76c04b11c1d28771872b3fbdb43446a
Marcus Näslund <marcus.naslund@kognity.com>
2023-03-13 15:20:40 +0100

    books and slider

--- src/script.js
+++ src/script.js
@@ -7,6 +7,7 @@ const statsParagraph = document.querySelector("#stats");
 const highScoreTable = document.querySelector("#highscore table");
 const rangeSelector = document.querySelector("#myRange")
 const descriptor = document.querySelector("#descriptor")
+const secondsdescriptor = document.querySelector("#secondsdescriptor")
 const descSpan = document.querySelector("span#desc")
 const valueSpan = document.querySelector("span#value")
 const indexSpan = document.querySelector("span#index")
@@ -18,6 +19,10 @@ const ctrGuess = document.querySelector("div#guess")
 const ctrGamestats = document.querySelector("div#gamestats")
 const ctrHighscore = document.querySelector("div#highscore")
 
+const divBooks = document.querySelector("div#bookad");
+
+const btnStart = document.querySelector("#start button");
+
 // =================
 // Game logic
 
@@ -26,6 +31,7 @@ var user_id = null;
 let generated_id = null;
 let generated_string = null;
 let game_index = 0;
+let logValue = 1;
 
 // =================
 // Post-game logic
@@ -36,6 +42,11 @@ let stats = {};
 
 // =================
 // Helpers
+
+function roundDecimals(number, precision) {
+    let rounder = Math.pow(10, precision);
+    return Math.round(number * rounder) / rounder;
+}
 
 function encodeQueryData(url, data) {
     const ret = [];
@@ -85,24 +96,111 @@ function playAgain() {
     start();
 }
 
+const TIME_MAPPING = [
+    {
+        maximum: 60*2,
+        conversion: 1,
+        decimals: 0,
+        suffix: 'sekunder',
+    },
+    {
+        maximum: 60*60*2,
+        conversion: 60,
+        decimals: 2,
+        suffix: 'minuter'
+    },
+    {
+        maximum: 60*60*24*2,
+        conversion: 60*60,
+        decimals: 2,
+        suffix: 'timmar'
+    },
+    {
+        maximum: 60*60*24*365,
+        conversion: 60*60*24,
+        decimals: 2,
+        suffix: 'dygn'
+    },
+    {
+        maximum: 60*60*24*365*100,
+        conversion: 60*60*24*365,
+        decimals: 2,
+        suffix: 'år'
+    },
+    {
+        maximum: 60*60*24*365*100*100000,
+        conversion: 60*60*24*365*100,
+        decimals: 2,
+        suffix: 'århundraden'
+    },
+    {
+        maximum: null,
+        conversion: 60*60*24*365*14700000000,
+        decimals: 3,
+        suffix: 'x tiden sedan Big Bang'
+    },
+]
+
+const DISTANCE_MAPPING = [
+    {
+        maximum: null,
+        conversion: 1,
+        decimals: 1,
+        suffix: 'meter'
+    }
+]
+
+const THIRD_MAPPING = [
+    {
+        maximum: null,
+        conversion: 1,
+        decimals: 1,
+        suffix: 'yada'
+    }
+]
+
 function rangeInput() {
+    logValue = Math.pow(1.005, rangeSelector.value);
+    let baseValue = Math.round(logValue);
+
     if (game_index == 1) {
-        descriptor.innerText = prettyTimeOutput(rangeSelector.value);
+        descriptor.innerText = prettyOutput(logValue, TIME_MAPPING);
+        secondsdescriptor.innerText = `${baseValue} sekunder`;
     }
     else if (game_index == 2) {
-        descriptor.innerText = prettyDistanceOutput(rangeSelector.value);
+        descriptor.innerText = prettyOutput(logValue, DISTANCE_MAPPING);
+        secondsdescriptor.innerText = `${baseValue} meter`;
     }
     else if (game_index == 3) {
-        descriptor.innerText = prettyThirdOutput(rangeSelector.value);
+        descriptor.innerText = prettyOutput(logValue, THIRD_MAPPING);
+        secondsdescriptor.innerText = `${baseValue} yadas`;
     }
 }
 
-function prettyTimeOutput(value) {
-    // TODO depend on type
+function prettyOutput(value, mapping) {
+    for (const {maximum, conversion, decimals, suffix} of mapping) {
+        if (maximum == null || value < maximum) {
+            let prettyValue = roundDecimals(value / conversion, decimals);
+            return `${prettyValue} ${suffix}`;
+        }
+    }
 
-    // if (seconds < 60) {
-        return `${value} sekunder`
+    // if (value < 60*60*24*2) {
+    //     let hours = roundDecimals(value / (60*60), 2);
+    //     return `${hours} timmar`;
     // }
+    // if (value < 60*60*24*365*1000) {
+    //     let years = roundDecimals(value / (60*60*24*365), 2);
+    //     return `${years} år`;
+    // }
+    // if (value < 60*60*24*365*100*100000) {
+    //     let centuries = roundDecimals(value / (60*60*24*365*100), 2);
+    //     return `${centuries} århundraden`;
+    // }
+
+    // let universes = roundDecimals(value / (60*60*24*365*15000000000), 3);
+    // return `${universes} x universums livstid`;
+
     // const totalMs = seconds * 1000;
     // const result = new Date(totalMs).toISOString().slice(11, 19);
 
@@ -144,8 +242,8 @@ function updateUI() {
 
     // Game info
     const descs = {
-        1: 'Om du hör en decimal varje sekund, hur länge måste du vänta innan du hör',
-        2: 'Om du går en meter för varje decimal, hur långt måste du gå innan du hör',
+        1: 'En person läser upp \\(\\pi\\) för dig. Om du får höra en decimal i taget, varje sekund, utan uppehåll, hur länge måste du vänta innan du hör',
+        2: 'Någon har ritat upp \\(\\pi\\) längs en raksträcka. Om varje decimal tar upp bara en millimeter, hur långt måste du vandra innan du stöter på',
         3: 'yada yada yada',
     };
     descSpan.innerText = descs[game_index];
@@ -157,9 +255,14 @@ function updateUI() {
 // ==================
 // Gameplay
 
+function editingName() {
+    btnStart.disabled = nameInput.value.length < 3 && nameInput.value.length < 20;
+}
+
 function start() {
     const name = nameInput.value;
     const url = encodeQueryData('start', {name, user_id});
+    btnStart.disabled = true;
 
     fetch(url)
         .then((response) => response.json())
@@ -168,7 +271,10 @@ function start() {
             game_id = data.game_id;
             user_id = data.user_id;            
         })
-        .then(next);    
+        .then(next)
+        .finally(() => {
+            btnStart.disabled = false;
+        });    
 }
 
 function next() {
@@ -178,11 +284,12 @@ function next() {
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
+            console.log(data);
+
             game_index++;
             rangeSelector.value = 0;
             rangeInput();
 
-            console.log(data);
             if (data.status == "done") {
                 getGameStats();
                 return;
@@ -197,7 +304,7 @@ function guess() {
     const guess = rangeSelector.value;
 
     const url = encodeQueryData('guess', {
-        user_id, game_id, generated_id, guess
+        user_id, game_id, generated_id, guess: Math.round(logValue)
     });
 
     fetch(url)
@@ -235,7 +342,9 @@ function getHighscore() {
         .then((data) => {
             console.log(data);
             highscore = data;
+            divBooks.classList.add('visible')
         })
         .then(updateUI);
 }
 
+updateUI();
